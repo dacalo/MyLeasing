736d9f5c39269b84d9d800064cf9545273942827
dacalo <dacalo.soporte@gmail.com>
2019-09-11 13:08:37 -0500

    Create Login App

---
+++ MyLeasing.Common/Models/TokenResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace MyLeasing.Common.Models
+{
+    using System;
+
+    public class TokenResponse
+    {
+        public string Token { get; set; }
+
+        public DateTime Expiration { get; set; }
+
+        public DateTime ExpirationLocal => Expiration.ToLocalTime();
+    }
+}
