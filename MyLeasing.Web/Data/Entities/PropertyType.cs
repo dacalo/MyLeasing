3677aff53212f58596a9a9a35f0bb5fcf17b0b7c
dacalo <dacalo.soporte@gmail.com>
2019-09-16 12:56:51 -0500

    Add comments to PropertyType

--- MyLeasing.Web/Data/Entities/PropertyType.cs
+++ MyLeasing.Web/Data/Entities/PropertyType.cs
@@ -4,8 +4,14 @@
     using System.ComponentModel.DataAnnotations;
     public class PropertyType
     {
+        /// <summary>
+        /// The Id
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// The Name FunctionalityC
+        /// </summary>
         [Display(Name = "Property Type*")]
         [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
         [Required(ErrorMessage = "The field {0} is mandatory.")]
